f1ed4d8643ca979a4a3655f5aeefa88e18c2dc7a
Marcia Tejeda <marcia.tejeda@unq.edu.ar>
2017-11-13 18:10:35 -0300

    Starting

--- src/main/java/org/unq/compiler/remolacha/app/App.java
+++ src/main/java/org/unq/compiler/remolacha/app/App.java
@@ -1,9 +1,13 @@
-package org.unq.compiler.remolacha.compiler;
+package org.unq.compiler.remolacha.app;
+import org.unq.compiler.remolacha.grammar.*;
+import org.unq.compiler.remolacha.grammar.Class;
 
 /**
  * Created by mtejeda on 13/11/17.
  */
 public class App {
 
-    
+    public static void main(String[] args) {
+      //  Class contador = new Class("Contador". )
+    }
 }
