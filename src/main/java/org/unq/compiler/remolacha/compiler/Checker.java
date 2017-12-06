d88bec71e2497df5fd5a307c7caaa678e5301997
marcia-t <tejedamarcia@gmail.com>
2017-12-06 02:25:03 -0300

    wip: some checks

--- src/main/java/org/unq/compiler/remolacha/compiler/Checker.java
+++ src/main/java/org/unq/compiler/remolacha/compiler/Checker.java
@@ -1,6 +1,9 @@
 package org.unq.compiler.remolacha.compiler;
 
-import org.unq.compiler.remolacha.grammar.Program;
+import org.unq.compiler.remolacha.grammar.*;
+import org.unq.compiler.remolacha.grammar.Class;
+
+import java.util.ArrayList;
 
 /**
  * Created by mtejeda on 14/11/17.
@@ -40,15 +43,33 @@ public class Checker {
     }
 
     private boolean notSameMethodAndSameSelector(Program p) {
-        return true;
+
+        return  true;
     }
 
     private boolean notClassRepeated(Program p) {
+        ArrayList<String> classes = new ArrayList<>();
+        for (Class c :
+                p.getClasses()) {
+            if (classes.contains(c.getId())) {
+                System.out.println("Hay dos clases con el mismo nombre, se aborta la compilación");
+                return false;
+            }
+            else classes.add(c.getId());
+        }
         return true;
     }
 
     private boolean existsMain(Program p) {
-        return true;
+        for (Class c : p.getClasses()) {
+            if (c.getId().equals("Main")) {
+                for (Method m : c.getMethods()) {
+                    if (m.getId().equals("main"))  return true;
+                }
+            }
+        }
+        System.out.println("El programa no tiene una clase main o bien la misma no tiene el método 'main' implementado");
+        return false;
     }
 
 }
